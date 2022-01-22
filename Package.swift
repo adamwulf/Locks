7e9c44ef246691366c9a43b8def9edc155889d8e
Adam Wulf <adam.wulf@gmail.com>
2022-01-21 21:59:02 -0600

    Better availability check

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Locks",
+    platforms: [.iOS(.v10), .macOS(.v10_12)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
--- Sources/Locks/SpinLock.swift
+++ Sources/Locks/SpinLock.swift
@@ -7,7 +7,6 @@
 
 import Foundation
 
-@available(macOS 10.12, *)
 public class SpinLock: NSLocking {
     private var unfairLock = os_unfair_lock_s()
     private var locked = false
