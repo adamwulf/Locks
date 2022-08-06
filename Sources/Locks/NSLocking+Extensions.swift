aee54a7f91bbbf4eb5ed0e796d1c3c5921fb4e3f
Adam Wulf <adam.wulf@gmail.com>
2022-08-06 14:16:23 -0500

    Adding extension for locking around a block of any NSLocking

---
+++ Sources/Locks/NSLocking+Extensions.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  
+//
+//  Created by Adam Wulf on 8/6/22.
+//
+
+import Foundation
+
+public extension NSLocking {
+    @discardableResult
+    func lockDuring<T>(block : () -> T) -> T {
+        self.lock()
+        defer {
+            self.unlock()
+        }
+        return block()
+    }
+}
